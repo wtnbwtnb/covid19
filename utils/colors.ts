ea7ce02532f2432c4d1589da3170d01fcc9dcde6
wanwanland <wanwanland@users.noreply.github.com>
2020-04-05 13:17:13 +0000

    deploy: 68cc95d12e979f815061f04bbb2f85bb194612fc

--- utils/colors.ts
+++
@@ -1,4 +0,0 @@
-export const single: string = '#006400'
-export const double: string[] = ['#006400', '#1B75BC']
-export const triple: string[] = ['#00441B', '#006400', '#1B75BC']
-export const quadruple: string[] = ['#00441B', '#006400', '#1B75BC', '#505B00']
